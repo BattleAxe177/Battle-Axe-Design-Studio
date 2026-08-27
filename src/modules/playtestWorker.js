aceefaaba6e7bf16f20ad4ce8fa85f69e08f9cb1
BattleAxe177 <scott.miquelon@gmail.com>
2026-08-27 07:55:11 -0400

    Battle Axe Design Studio Revert to v0.6.5.0

--- src/modules/playtestWorker.js
+++ src/modules/playtestWorker.js
@@ -1,4 +1,4 @@
-import { runPlaytest, runBatch } from './playtestEngine.js?v=0.6.6.0';
+import { runPlaytest, runBatch } from './playtestEngine.js?v=0.6.5.0';
 self.onmessage=(event)=>{
   const {id,kind,state,settings}=event.data||{};
   try{
